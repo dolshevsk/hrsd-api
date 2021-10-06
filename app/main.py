4f1ff898ded58dc04c55088444815856ea78efc8
dolshevsk <olshevskiydk@gmail.com>
2021-10-06 02:49:58 +0200

    feature: add postgres support

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
+import asyncio
+
 from aiohttp import web
 
 from routes import setup_routes
+from storage import get_conn_poll, ping
 
 
-def main():
+async def init_app():
+    pool = await get_conn_poll()
+    await ping(pool)
+
     app = web.Application()
     setup_routes(app)
-    web.run_app(app)
+    return app
 
 
 if __name__ == '__main__':
-    main()
+    app = asyncio.get_event_loop().run_until_complete(init_app())
+    web.run_app(app)
